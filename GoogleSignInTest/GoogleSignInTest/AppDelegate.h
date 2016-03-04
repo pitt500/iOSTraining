a050a42084e4aa404f6e8b0b79fbf7c6981ee3f1
pittNearsoft <projas@nearsoft.com>
2016-03-04 13:22:50 -0700

    Add my own GoogleSignInTest project

---
+++ GoogleSignInTest/GoogleSignInTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleSignInTest
+//
+//  Created by projas on 3/4/16.
+//  Copyright © 2016 projas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
