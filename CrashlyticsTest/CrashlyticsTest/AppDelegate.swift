8ec07f77ff366785758e9997013441f0dcc1e490
pittNearsoft <projas@nearsoft.com>
2016-04-19 17:10:03 -0700

    Remove logUser method

--- CrashlyticsTest/CrashlyticsTest/AppDelegate.swift
+++ CrashlyticsTest/CrashlyticsTest/AppDelegate.swift
@@ -21,18 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         Fabric.with([Crashlytics.self])
         // TODO: Move this to where you establish a user session
-        self.logUser()
-
 
         return true
-    }
-    
-    func logUser() {
-        // TODO: Use the current user's information
-        // You can call any combination of these three methods
-        Crashlytics.sharedInstance().setUserEmail("user@fabric.io")
-        Crashlytics.sharedInstance().setUserIdentifier("12345")
-        Crashlytics.sharedInstance().setUserName("Test User")
     }
 
 
